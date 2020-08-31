b928174ff52714c9729b45ddc533150a160d1e33
Akramion <leha.bushuev@gmail.com>
2020-08-31 11:59:56 +0300

    guide

---
+++ Assets/Scripts/Guide/GuideButton.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GuideButton : MonoBehaviour
+{
+    public GameObject[] slides;
+    private int count = 0;
+
+    public void NextSlide()
+    {
+        if(count == 13)
+        {
+            return;
+        }
+
+        slides[count].SetActive(false);
+
+        count++;
+        slides[count].SetActive(true);
+    }
+}
